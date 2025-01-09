34bc4edc51c870b0795f6209606d6fd3fe126627
Pranita Devadkar <125338450+Pranitadevadkar16@users.noreply.github.com>
2025-01-09 12:36:55 +0530

    Add files via upload

---
+++ Frontend/src/components/TaskList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const response = await axios.get("http://localhost:5000/api/tasks");
+    setTasks(response.data);
+  };
+
+  const addTask = async () => {
+    const response = await axios.post("http://localhost:5000/api/tasks", {
+      title: newTask,
+      description: "Default description",
+    });
+    setTasks([...tasks, response.data]);
+    setNewTask("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
